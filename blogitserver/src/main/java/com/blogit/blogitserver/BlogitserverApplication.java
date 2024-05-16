424cb66e6bb321f54661ad5acc019fe7b776c73a
bykbyk0401 <bykbyk0401@gmail.com>
2024-05-16 15:23:37 +0900

    setting: 프로젝트 세팅

---
+++ blogitserver/src/main/java/com/blogit/blogitserver/BlogitserverApplication.java
@@ -0,0 +1,13 @@
+package com.blogit.blogitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogitserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogitserverApplication.class, args);
+	}
+
+}
---
+++ blogitserver/src/test/java/com/blogit/blogitserver/BlogitserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogit.blogitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogitserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
